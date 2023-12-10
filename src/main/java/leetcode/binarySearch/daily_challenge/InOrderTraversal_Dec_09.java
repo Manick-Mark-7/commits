a3fe825d7d79281315d516a10c5ed012f621eab7
Manickavasagam S <manickavasagam.04.09@gmail.com>
2023-12-10 21:25:03 +0530

    Dec 10 , Subarray with sum zero

--- src/main/java/leetcode/binarySearch/daily_challenge/InOrderTraversal_Dec_09.java
+++ src/main/java/leetcode/binarySearch/daily_challenge/InOrderTraversal_Dec_09.java
@@ -1,23 +1,8 @@
 package leetcode.binarySearch.daily_challenge;
 
-
 import java.util.ArrayList;
 import java.util.List;
 
-class TreeNode{
-    int val;
-    TreeNode left;
-    TreeNode right;
-
-    public TreeNode(){}
-    public TreeNode(int val){this.val = val;}
-
-    public TreeNode(int val, TreeNode left, TreeNode right){
-        this.val = val;
-        this.left = left;
-        this.right = right;
-    }
-}
 
 //Inorder traversal of the binary tree
 public class InOrderTraversal_Dec_09 {
