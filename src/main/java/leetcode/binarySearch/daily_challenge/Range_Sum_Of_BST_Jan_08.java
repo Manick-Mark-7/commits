2b8ecd570194744e6c570663d5187ddc89bc9cbb
Manickavasagam S <manickavasagam.04.09@gmail.com>
2024-01-08 21:39:01 +0530

    Range_Sum_Of_BST_Jan 08

---
+++ src/main/java/leetcode/binarySearch/daily_challenge/Range_Sum_Of_BST_Jan_08.java
@@ -0,0 +1,17 @@
+package src.main.java.leetcode.binarySearch.daily_challenge;
+
+public class Range_Sum_Of_BST_Jan_08 {
+
+    public int rangeSumBST(TreeNode root, int low, int high) {
+        if (root == null) {
+            return 0;
+        }
+
+        int currentVal = (root.val >= low && root.val <= high) ? root.val : 0;
+
+        int leftSum = rangeSumBST(root.left, low, high);
+        int rightSum = rangeSumBST(root.right, low, high);
+
+        return currentVal + leftSum + rightSum;
+    }
+}
